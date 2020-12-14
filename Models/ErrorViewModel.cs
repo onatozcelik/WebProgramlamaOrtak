50f9dda6e6fec912fab00e5c67076ca5fb6317cb
onatozcelik <58370802+onatozcelik@users.noreply.github.com>
2020-12-14 21:30:58 +0300

    proje oluşturuldu

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheBookstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
